7fcd01132818f32ff5b4a11a33f309aaa1a5360c
Alysson Victor <aly@pop-os.localdomain>
2022-09-04 20:26:48 -0300

    [DependencyInversion] Add default classes BudgeReport and MYSQLDatabase

---
+++ DependencyInversion/BudgetReport.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * BudgetReport class
+ */
+class BudgetReport
+{
+    /**
+     * @var MYSQLDatabase
+     */
+    private MYSQLDatabase $database;
+
+    /**
+     * @return void
+     */
+    public function open(): void
+    {
+        // Do something to open a report
+    }
+
+    /**
+     * @return void
+     */
+    public function save(): void
+    {
+        // Do something to save a new report
+    }
+}
---
+++ DependencyInversion/MYSQLDatabase.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MYSQLDatabase class
+ */
+class MYSQLDatabase
+{
+    /**
+     * @return void
+     */
+    public function insert()
+    {
+        // Do something to perform a data insert on database
+    }
+
+    /**
+     * @return void
+     */
+    public function update()
+    {
+        // Do something to perform a data update on database
+    }
+
+    /**
+     * @return void
+     */
+    public function delete()
+    {
+        // Do something to perform a data delete on database
+    }
+}
